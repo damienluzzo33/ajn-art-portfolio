1eb4e2d6e09e06953bbab86e3076d4ff80d38d96
Damien Luzzo <damienluzzo33@gmail.com>
2022-03-16 23:55:50 -0500

    add apollo configuration to App component and set up react router switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,52 @@
 import './App.css';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import AuthService from './utils/auth';
+
+//* Construct GraphQL endpoint
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+//* Construct middleware for every authorization request
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <h1>It Renders!</h1>
-            </div>
-            <Home />
-        </div>
+        <ApolloProvider client={client}>
+            <Router>
+                <div className="App">
+                    <div>
+                        <h1>It Renders!</h1>
+                    </div>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </ApolloProvider>
     );
 }
 
